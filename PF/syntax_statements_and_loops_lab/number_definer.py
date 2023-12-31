36a5afa12b41a15f35b70675724047185acf4c07
simonalev03 <simon.alev03@gmail.com>
2023-12-31 19:01:18 +0200

    added new exercise, fixed the others

---
+++ PF/syntax_statements_and_loops_lab/number_definer.py
@@ -0,0 +1,20 @@
+# Read user input
+number = float(input())
+
+# Logic
+if number > 0:
+    if number < 1:
+        print("small positive")
+    elif number > 1000000:
+        print("large positive")
+    else:
+        print("positive")
+elif number < 0:
+    if abs(number) < 1:
+        print("small negative")
+    elif abs(number) > 1000000:
+        print("large negative")
+    else:
+        print("negative")
+else:
+    print("zero")
