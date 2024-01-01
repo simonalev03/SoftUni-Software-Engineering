2407d374cce1e98bdcbddaacca5439440f1dcada
simonalev03 <simon.alev03@gmail.com>
2024-01-01 17:56:14 +0200

    added new exercise

--- PF/syntax_statements_and_loops_exercise/chat_codes.py
+++ PF/syntax_statements_and_loops_exercise/chat_codes.py
@@ -2,12 +2,12 @@ number_of_messages = int(input())
 
 message = ""
 for message_number in range(number_of_messages):
-    message_type = int(input())
-    if message_type == 88:
+    chat_code = int(input())
+    if chat_code == 88:
         message = "Hello"
-    elif message_type == 86:
+    elif chat_code == 86:
         message = "How are you?"
-    elif message_type < 88:
+    elif chat_code < 88:
         message = "GREAT!"
     else:
         message = "Bye."
