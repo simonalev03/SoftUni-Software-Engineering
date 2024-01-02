5c6ceb8fe0797457414bcf39e9ec951a79edefba
simonalev03 <simon.alev03@gmail.com>
2024-01-02 11:58:30 +0200

    added new exercise

---
+++ PF/syntax_statements_and_loops_exercise/how_much_coffee_you_need.py
@@ -0,0 +1,26 @@
+total_coffee = 0
+while True:
+    command = input()
+    if command == "END":
+        break
+    elif command.lower() == "coding":
+        if command.isupper():
+            total_coffee += 2
+        else:
+            total_coffee += 1
+    elif command.lower() == "dog" or command.lower() == "cat":
+        if command.isupper():
+            total_coffee += 2
+        else:
+            total_coffee += 1
+    elif command.lower() == "movie":
+        if command.isupper():
+            total_coffee += 2
+        else:
+            total_coffee += 1
+    else:
+        continue
+if total_coffee > 5:
+    print("You need extra sleep")
+else:
+    print(total_coffee)
